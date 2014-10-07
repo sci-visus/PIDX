fc69517a14b7549a68c060ed388297a41ba6efd8
Valerio Pascucci <pascucci@acm.org>
2014-10-07 13:53:20 -0600

    first edit

--- pidx/PIDX.h
+++ pidx/PIDX.h
@@ -3,7 +3,7 @@
  **  Copyright (c) 2010-2014 University of Utah     **
  **  Scientific Computing and Imaging Institute     **
  **  72 S Central Campus Drive, Room 3750           **
- **  Salt Lake City, UT 84112                       **
+ **  Salt Lake City, UT 84112                       ***
  **                                                 **
  **  PIDX is licensed under the Creative Commons    **
  **  Attribution-NonCommercial-NoDerivatives 4.0    **
