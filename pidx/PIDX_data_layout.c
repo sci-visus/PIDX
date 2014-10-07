12d97ff37a46fed4aa045a11ee92eaa60b4d8921
sidharth kumar <kumar.sidharth87@gmail.com>
2014-10-07 23:57:18 +0300

    adding files

---
+++ pidx/PIDX_data_layout.c
@@ -0,0 +1,22 @@
+/******************************************************
+ **  PIDX Parallel I/O Library                      **
+ **  Copyright (c) 2010-2014 University of Utah     **
+ **  Scientific Computing and Imaging Institute     **
+ **  72 S Central Campus Drive, Room 3750           **
+ **  Salt Lake City, UT 84112                       **
+ **                                                 **
+ **  PIDX is licensed under the Creative Commons    **
+ **  Attribution-NonCommercial-NoDerivatives 4.0    **
+ **  International License. See LICENSE.md.         **
+ **                                                 **
+ **  For information about this project see:        **
+ **  http://www.cedmav.com/pidx                     **
+ **  or contact: pascucci@sci.utah.edu              **
+ **  For support: PIDX-support@visus.net            **
+ **                                                 **
+ *****************************************************/
+
+#include "PIDX_data_layout.h"
+
+PIDX_data_layout PIDX_row_major = 0;
+PIDX_data_layout PIDX_column_major = 1;
