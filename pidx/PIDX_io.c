5871ed42ae30b008378fcf7d1a18356dd1c51eb2
sidharth kumar <kumar.sidharth87@gmail.com>
2015-01-04 19:31:51 -0700

    debug for one large file on mira

--- pidx/PIDX_io.c
+++ pidx/PIDX_io.c
@@ -585,7 +585,7 @@ int PIDX_io_aggregated_IO(PIDX_io_id io_id, Agg_buffer agg_buffer, int MODE)
         mpi_ret = MPI_File_write_at(fh, data_offset, agg_buffer->buffer, ((io_id->idx_derived_ptr->existing_blocks_index_per_file[agg_buffer->file_number]) * (io_id->idx_derived_ptr->samples_per_block/io_id->idx_derived_ptr->aggregation_factor)  * (io_id->idx_ptr->variable[agg_buffer->var_number]->bits_per_value/8)) , MPI_BYTE, &status);
         if (mpi_ret != MPI_SUCCESS) 
         {
-          fprintf(stderr, "Data offset = %d %lld [%s] [%d] MPI_File_open() failed.\n", (int) data_offset, (long long)data_offset __FILE__, __LINE__);
+          fprintf(stderr, "Data offset = %d %lld [%s] [%d] MPI_File_open() failed.\n", (int) data_offset, (long long)data_offset, __FILE__, __LINE__);
           return -1;
         }
         
