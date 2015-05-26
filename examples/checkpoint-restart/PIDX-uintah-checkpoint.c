fe2bc29d5b84bf88cb953a127b188b8249161d93
sidharth87 <kumar.sidharth87@gmail.com>
2015-05-26 00:35:26 -0600

    tutorial based executables (onlt the skeleton)

---
+++ examples/checkpoint-restart/PIDX-uintah-checkpoint.c
@@ -0,0 +1,49 @@
+/*****************************************************
+ **  PIDX Parallel I/O Library                      **
+ **  Copyright (c) 2010-2014 University of Utah     **
+ **  Scientific Computing and Imaging Institute     **
+ **  72 S Central Campus Drive, Room 3750           **
+ **  Salt Lake City, UT 84112                       **
+ **                                                 **
+ **  PIDX is licensed under the Creative Commons    **
+ **  Attribution-NonCommercial-NoDerivatives 4.0    **
+ **  International License. See LICENSE.md.         **
+ **                                                 **
+ **  For information about this project see:        **
+ **  http://www.cedmav.com/pidx                     **
+ **  or contact: pascucci@sci.utah.edu              **
+ **  For support: PIDX-support@visus.net            **
+ **                                                 **
+ *****************************************************/
+
+#include <getopt.h>
+#include <string.h>
+#include <assert.h>
+#include <errno.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdlib.h>
+
+#if PIDX_HAVE_MPI
+  #include <mpi.h>
+#endif
+
+
+/// main
+int main(int argc, char **argv)
+{
+
+  int nprocs, rank;
+#if PIDX_HAVE_MPI
+  /// MPI initialization
+  MPI_Init(&argc, &argv);
+  MPI_Comm_size(MPI_COMM_WORLD, &nprocs);
+  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+#endif
+
+
+#if PIDX_HAVE_MPI
+  MPI_Finalize();
+#endif
+  return 0;
+}
