d347a6c10874e9dcb020e49be31fab3c93c19fdc
sidharth87 <kumar.sidharth87@gmail.com>
2016-03-06 18:27:54 -0700

    PIDX structural changes + new code

---
+++ pidx/data_handle/PIDX_blocks.c
@@ -0,0 +1,361 @@
+/*****************************************************
+ **  PIDX Parallel I/O Library                      **
+ **  Copyright (c) 2010-2014 University of Utah     **
+ **  Scientific Computing and Imaging Institute     **
+ **  72 S Central Campus Drive, Room 3750           **
+ **  Salt Lake City, UT 84112                       **
+ **                                                 **
+ **  PIDX is licensed under the Creative Commons    **
+ **  Attribution-NonCommercial-NoDerivatives 4.0    **
+ **  International License. See LICENSE.md.         **
+ **                                                 **
+ **  For information about this project see:        **
+ **  http://www.cedmav.com/pidx                     **
+ **  or contact: pascucci@sci.utah.edu              **
+ **  For support: PIDX-support@visus.net            **
+ **                                                 **
+ *****************************************************/
+ 
+
+/*
+Block size = 1024
+Bits per block = 10
+Blocks per file = 4
+Dataset size = 32 x 32 x 32 (32768)
+maxh = 16
+#of agg levels = maxh - (bits_per_block + log2(blocks per file))
+Level #Elements [#Block_id] (HZ_start HZ_end) (HZ_start_form HZ_end_form)
+0 1 [0] (0,0) [0]
+1 1 [0] (1,1) [0] (pow(2, level-1), pow(2, level) - 1)
+2 2 [0] (2,3) [0]
+3 4 [0] (4,7) [0]
+4 8 [0] (8,15) [0]
+5 16 [0] (16, 31) [0]
+6 32 [0] (32, 63) [0]
+7 64 [0] (64, 127) [0]
+8 128 [0] (128, 255) [0]
+9 256 [0] (256, 511) [0]
+10 512 [0] (512, 1023) [0]
+11 1024 [1] (1024, 2047) [0]
+12 2048 [2, 3] (2048, 4095) [0] ----> 1
+
+13 4096 [4, 5, 6, 7] (4096, 8191) [1]  ----> 2
+14 8192 [8, 9, 10, 11, 12, 13, 14, 15] (8192, 16383) [2, 3]  ----> 3, 4
+15 16384 [16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31] (16384, 32767) [4, 5, 6, 7]  ----> 5, 6, 7, 8
+*/
+
+#include "../PIDX_inc.h"
+
+const int PIDX_default_bits_per_block       = 15;
+const int PIDX_default_blocks_per_file      = 256;
+
+
+int PIDX_blocks_initialize_layout (PIDX_block_layout layout, int resolution_from, int resolution_to, int maxh, int bits_per_block)
+{
+  int ctr = 1, j;
+
+  layout->maxh = maxh;
+  layout->resolution_from = resolution_from;
+  layout->resolution_to = resolution_to;
+  layout->bits_per_block = bits_per_block;
+
+  if (layout->maxh < bits_per_block + 1)
+    layout->maxh = bits_per_block + 1;
+
+  //levels = (resolution_to - resolution_from);
+  //printf("levels = %d\n (%d %d)\n", levels, layout->resolution_from, layout->resolution_to);
+
+  layout->hz_block_number_array = malloc(sizeof(int*) * layout->maxh);
+  memset(layout->hz_block_number_array, 0, sizeof(int*) * layout->maxh);
+
+  //if (resolution_from <= bits_per_block)
+  //{
+    for (j = 0 ; j < bits_per_block + 1 ; j++)
+    {
+      layout->hz_block_number_array[j] = malloc(sizeof(int));
+      memset(layout->hz_block_number_array[j], 0, sizeof(int));
+    }
+
+    ctr = 1;
+    for (j = bits_per_block + 1 ; j < layout->maxh ; j++)
+    {
+      layout->hz_block_number_array[j] = malloc(sizeof(int) * ctr);
+      memset(layout->hz_block_number_array[j], 0, sizeof(int) * ctr);
+      ctr = ctr * 2;
+    }
+  //}
+  /*
+  else
+  {
+    ctr = 1;
+    for (j = bits_per_block + 1 ; j < resolution_to ; j++)
+    {
+      if (j > resolution_from)
+      {
+        layout->hz_block_number_array[j] = malloc(sizeof(int) * ctr);
+        memset(layout->hz_block_number_array[j], 0, sizeof(int) * ctr);
+      }
+      ctr = ctr * 2;
+    }
+  }
+  */
+
+  return PIDX_success;
+}
+
+
+///
+int PIDX_blocks_create_layout (int bounding_box[2][5], int maxH, const char* bitPattern, PIDX_block_layout layout, int res_from, int res_to)
+{
+  int m = 0, n_blocks = 1, t = 0, block_number = 1;
+  int64_t hz_from = 0, hz_to = 0;
+  int64_t *ZYX_from, *ZYX_to;
+
+  int adjusted_res_to = layout->resolution_to;
+  if (layout->resolution_to > maxH - res_to)
+  {
+    adjusted_res_to = maxH - res_to;//layout->resolution_to - res_to;
+//    printf("adjusted %d [%d %d] maxh (%d %d) res_to %d\n", layout->resolution_to, layout->resolution_from, adjusted_res_to, layout->maxh, maxH, res_to);
+  }
+  //else
+  //  printf("N adjusted %d [%d %d] maxh (%d %d) res_to %d\n", layout->resolution_to, layout->resolution_from, adjusted_res_to, layout->maxh, maxH, res_to);
+
+  ZYX_to = (int64_t*) malloc(sizeof(int64_t) * PIDX_MAX_DIMENSIONS);
+  ZYX_from = (int64_t*) malloc(sizeof(int64_t) * PIDX_MAX_DIMENSIONS);
+
+  //printf("res from to to : %d %d\n", layout->resolution_from, layout->resolution_to);
+  if (layout->resolution_from <= layout->bits_per_block)
+  {
+    for (m = layout->resolution_from ; m <= layout->bits_per_block; m++)
+    {
+      if (m == 0)
+      {
+        hz_from = 0;
+        hz_to = 0;
+      }
+      else
+      {
+        hz_from = (int64_t)pow(2, m-1);
+        hz_to = (int64_t)pow(2, m) - 1;
+      }
+
+      memset(ZYX_to, 0, sizeof(int64_t) * PIDX_MAX_DIMENSIONS);
+      memset(ZYX_from, 0, sizeof(int64_t) * PIDX_MAX_DIMENSIONS);
+
+      Hz_to_xyz(bitPattern, maxH - 1, hz_from, ZYX_from);
+      Hz_to_xyz(bitPattern, maxH - 1, hz_to, ZYX_to);
+
+      if (ZYX_to[0] >= bounding_box[0][0] && ZYX_from[0] < bounding_box[1][0] && ZYX_to[1] >= bounding_box[0][1] && ZYX_from[1] < bounding_box[1][1] && ZYX_to[2] >= bounding_box[0][2] && ZYX_from[2] < bounding_box[1][2] && ZYX_to[3] >= bounding_box[0][3] && ZYX_from[3] < bounding_box[1][3] && ZYX_to[4] >= bounding_box[0][4] && ZYX_from[4] < bounding_box[1][4])
+        layout->hz_block_number_array[m][0] = 0;
+    }
+
+    for (m = layout->bits_per_block + 1 ; m < adjusted_res_to; m++)
+    {
+      n_blocks = pow(2, (m - (layout->bits_per_block + 1)));
+      for (t = 0 ; t < n_blocks ; t++)
+      {
+        hz_from = (int64_t)(block_number) * pow(2, layout->bits_per_block);
+        hz_to = (int64_t)((block_number + 1) * pow(2, layout->bits_per_block)) - 1;
+      
+        memset(ZYX_to, 0, sizeof(int64_t) * PIDX_MAX_DIMENSIONS);
+        memset(ZYX_from, 0, sizeof(int64_t) * PIDX_MAX_DIMENSIONS);
+      
+        Hz_to_xyz(bitPattern, maxH - 1, hz_from, ZYX_from);
+        Hz_to_xyz(bitPattern, maxH - 1, hz_to, ZYX_to);
+      
+        //printf("m = %d\n", m);
+        if (ZYX_to[0] >= bounding_box[0][0] && ZYX_from[0] < bounding_box[1][0] && ZYX_to[1] >= bounding_box[0][1] && ZYX_from[1] < bounding_box[1][1] && ZYX_to[2] >= bounding_box[0][2] && ZYX_from[2] < bounding_box[1][2] && ZYX_to[3] >= bounding_box[0][3] && ZYX_from[3] < bounding_box[1][3] && ZYX_to[4] >= bounding_box[0][4] && ZYX_from[4] < bounding_box[1][4])
+          layout->hz_block_number_array[m][t] = block_number;
+      
+        block_number++;
+      }
+    }
+  }
+  else
+  {
+    for (m = layout->bits_per_block + 1 ; m < adjusted_res_to; m++)
+    {
+      n_blocks = pow(2, (m - (layout->bits_per_block + 1)));
+      if (m >= layout->resolution_from)
+      {
+        for (t = 0 ; t < n_blocks ; t++)
+        {
+          hz_from = (int64_t)(block_number) * pow(2, layout->bits_per_block);
+          hz_to = (int64_t)((block_number + 1) * pow(2, layout->bits_per_block)) - 1;
+
+          memset(ZYX_to, 0, sizeof(int64_t) * PIDX_MAX_DIMENSIONS);
+          memset(ZYX_from, 0, sizeof(int64_t) * PIDX_MAX_DIMENSIONS);
+
+          Hz_to_xyz(bitPattern, maxH - 1, hz_from, ZYX_from);
+          Hz_to_xyz(bitPattern, maxH - 1, hz_to, ZYX_to);
+
+          if (ZYX_to[0] >= bounding_box[0][0] && ZYX_from[0] < bounding_box[1][0] && ZYX_to[1] >= bounding_box[0][1] && ZYX_from[1] < bounding_box[1][1] && ZYX_to[2] >= bounding_box[0][2] && ZYX_from[2] < bounding_box[1][2] && ZYX_to[3] >= bounding_box[0][3] && ZYX_from[3] < bounding_box[1][3] && ZYX_to[4] >= bounding_box[0][4] && ZYX_from[4] < bounding_box[1][4])
+            layout->hz_block_number_array[m][t] = block_number;
+
+          block_number++;
+        }
+      }
+      else
+        block_number = block_number + n_blocks;
+    }
+  }
+  free(ZYX_from);
+  ZYX_from = 0;
+  free(ZYX_to);
+  ZYX_to = 0;
+
+  return 0;
+}
+
+
+///
+void PIDX_blocks_print_layout(PIDX_block_layout layout)
+{
+  int ctr = 1, i, j;
+  int res_level = 0;
+  int res_index = 0;
+
+  printf("levels: [%d %d] [%d]\n", layout->resolution_from, layout->resolution_to, layout->maxh);
+  if (layout->resolution_from <= layout->bits_per_block)
+  {
+    for (i = layout->resolution_from; i <= layout->bits_per_block; i++)
+    {
+      printf("Number of blocks at level %d = %d :: ", i, ctr);
+      for (j = 0 ; j <  ctr; j++)
+      {
+        if (layout->hz_block_number_array[i][j] == 0)
+          res_level = 0;
+        else
+        {
+          res_level = log2 (layout->hz_block_number_array[i][j]) + 1 + layout->bits_per_block;
+          res_index = layout->hz_block_number_array[i][j] % ((int) pow(2, (res_level - 1 - layout->bits_per_block)));
+        }
+        //printf("%d ", layout->hz_block_number_array[i][j]);
+        printf("[%d (%d %d)] ", layout->hz_block_number_array[i][j], res_level, res_index);
+      }
+      printf("\n");
+    }
+
+    for (i = layout->bits_per_block + 1; i < layout->resolution_to; i++)
+    {
+      printf("Number of blocks at level %d = %d :: ", i, ctr);
+      for (j = 0 ; j <  ctr; j++)
+      {
+        if (layout->hz_block_number_array[i][j] == 0)
+          res_level = 0;
+        else
+        {
+          res_level = log2 (layout->hz_block_number_array[i][j]) + 1 + layout->bits_per_block;
+          res_index = layout->hz_block_number_array[i][j] % ((int) pow(2, (res_level - 1 - layout->bits_per_block)));
+        }
+        printf("[%d (%d %d)] ", layout->hz_block_number_array[i][j], res_level, res_index);
+        //printf("%d ", layout->hz_block_number_array[i][j]);
+      }
+
+      ctr = ctr * 2;
+      printf("\n");
+    }
+  }
+  else
+  {
+    for (i = layout->bits_per_block + 1; i < layout->resolution_to; i++)
+    {
+      if (i >= layout->resolution_from)
+      {
+        printf("Number of blocks at level %d = %d :: ", i, ctr);
+        for (j = 0 ; j <  ctr; j++)
+        {
+          if (layout->hz_block_number_array[i][j] == 0)
+            res_level = 0;
+          else
+          {
+            res_level = log2 (layout->hz_block_number_array[i][j]) + 1 + layout->bits_per_block;
+            res_index = layout->hz_block_number_array[i][j] % ((int) pow(2, (res_level - 1 - layout->bits_per_block)));
+          }
+          printf("[%d (%d %d)] ", layout->hz_block_number_array[i][j], res_level, res_index);
+        }
+      }
+      ctr = ctr * 2;
+      if (i > layout->resolution_from)
+        printf("\n");
+    }
+  }
+}
+
+
+///
+int PIDX_blocks_is_block_present(int block_number, PIDX_block_layout layout)
+{
+  int res_level = 0, res_index = 0;
+  
+  if (block_number == 0)
+    res_level = layout->bits_per_block;
+  else
+  {
+    res_level = log2 (block_number) + 1 + layout->bits_per_block;
+    res_index = block_number % ((int) pow(2, (res_level - 1 - layout->bits_per_block)));
+  }
+
+  if (res_level < layout->resolution_from || res_level >= layout->resolution_to)
+    return 0;
+
+  if (layout->hz_block_number_array[res_level][res_index] == block_number)
+    return 1;
+
+  return 0;
+}
+
+
+///
+int PIDX_blocks_find_negative_offset(int blocks_per_file, int block_number, PIDX_block_layout layout)
+{
+  //PIDX_blocks_print_layout(layout);
+  int b;
+  int file_no = block_number/blocks_per_file;
+  int block_offset = 0;
+  for (b = file_no * blocks_per_file ; b < block_number; b++)
+    if (!PIDX_blocks_is_block_present(b, layout))
+      block_offset++;
+  return block_offset;
+}
+
+
+///
+void PIDX_blocks_free_layout(PIDX_block_layout layout)
+{
+  int j = 0;
+  for (j = 0 ; j < layout->bits_per_block + 1 ; j++)
+  {
+    free(layout->hz_block_number_array[j]);
+    layout->hz_block_number_array[j] = 0;
+  }
+
+  for (j = layout->bits_per_block + 1 ; j < layout->maxh ; j++)
+  {
+    free(layout->hz_block_number_array[j]);
+    layout->hz_block_number_array[j] = 0;
+  }
+
+  free(layout->hz_block_number_array);
+  layout->hz_block_number_array = 0;
+
+  return;
+}
+
+void PIDX_free_layout(PIDX_block_layout layout)
+{
+  free(layout->file_bitmap);
+  layout->file_bitmap = 0;
+
+  free(layout->block_count_per_file);
+  layout->block_count_per_file = 0;
+
+  free(layout->existing_file_index);
+  layout->existing_file_index = 0;
+
+  free(layout->inverse_existing_file_index);
+  layout->inverse_existing_file_index = 0;
+
+  free(layout->file_index);
+  layout->file_index = 0;
+}
---
+++ pidx/data_handle/PIDX_idx_data_structs.h
@@ -0,0 +1,188 @@
+/*****************************************************
+ **  PIDX Parallel I/O Library                      **
+ **  Copyright (c) 2010-2014 University of Utah     **
+ **  Scientific Computing and Imaging Institute     **
+ **  72 S Central Campus Drive, Room 3750           **
+ **  Salt Lake City, UT 84112                       **
+ **                                                 **
+ **  PIDX is licensed under the Creative Commons    **
+ **  Attribution-NonCommercial-NoDerivatives 4.0    **
+ **  International License. See LICENSE.md.         **
+ **                                                 **
+ **  For information about this project see:        **
+ **  http://www.cedmav.com/pidx                     **
+ **  or contact: pascucci@sci.utah.edu              **
+ **  For support: PIDX-support@visus.net            **
+ **                                                 **
+ *****************************************************/
+
+/**
+ * \file PIDX_idx_data_structs.h
+ *
+ * \author Sidharth Kumar
+ * \author Cameron Christensen
+ *
+ * IDX related data structs:
+ * -- PIDX_variable
+ * -- idx_dataset
+ * -- idx_dataset_derived_metadata
+ */
+
+#ifndef __PIDX_IDX_DATA_STRUCTS_H
+#define __PIDX_IDX_DATA_STRUCTS_H
+
+#include "PIDX_memory_layout_data_structs.h"
+
+
+struct PIDX_timming_struct
+{
+   int header_counter;
+   double file_create_time;
+   double populate_idx_start_time;
+   double populate_idx_end_time;
+   double sim_start, sim_end;
+   double *write_init_start, *write_init_end;
+   double *startup_start, *startup_end;
+   double *init_start, *init_end;
+   double *rst_start, *rst_end;
+   double *hz_start, *hz_end;
+   double *cleanup_start, *cleanup_end;
+   double *finalize_start, *finalize_end;
+   double *chunk_start, *chunk_end;
+   double *buffer_start, *buffer_end;
+   double *compression_start, *compression_end;
+   double **agg_start, **agg_end;
+   double **agg_buf_start, **agg_buf_end;
+   double **io_per_process_start, **io_per_process_end;
+   double **io_start, **io_end;
+};
+typedef struct PIDX_timming_struct* PIDX_time;
+
+
+///
+struct PIDX_variable_struct
+{
+  // Metadata
+  int dump_meta_data_ON;                                                ///< Counter set if meta data dumping activated
+  
+  int io_state;
+
+  // General Info
+  char var_name[1024];                                                  ///< Variable name
+  int values_per_sample;                                                ///< Vector(3), scalar(1), or n
+  int bits_per_value;                                                   ///< Number of bits each need
+  PIDX_data_type type_name;                                                 ///< Name of the type uint8, bob
+  PIDX_data_layout data_layout;                                         ///< Row major or column major
+
+  // Memory layout (before, after HZ encoding phase)
+  int sim_patch_count;                                                  ///< The actual number of patches (application layout), most probably more than 1 in uintah
+  Ndim_patch sim_patch[1024];                                           ///< Pointer to the patches
+  HZ_buffer* hz_buffer;                                                 ///< HZ encoded buffer of the patches
+
+  // Memory layout before aggregation 
+  int patch_group_count;                                                ///< Number of groups of patches to be passed to aggregation phase
+  Ndim_patch_group* rst_patch_group;                                    ///< Pointer to the patch groups
+  Ndim_patch_group* chunk_patch_group;                                  ///< Pointer to the patch group after block restructuring
+
+  // Block level layout
+  PIDX_block_layout global_block_layout;                               ///< Block layout, specifically when variables might have different extents in the domain
+  PIDX_block_layout* block_layout_by_level;                            ///< Block layout, specifically when variables might have different extents in the domain
+
+  //Compression related
+  int lossy_compressed_block_size;                                      ///< The expected size of the compressed buffer
+};
+typedef struct PIDX_variable_struct* PIDX_variable;
+
+
+/// idx_file
+struct idx_file_struct
+{
+  int current_time_step;                                                ///< Time step tracker
+  
+  int variable_count;
+  int variable_index_tracker;
+  PIDX_variable variable[1024];
+  
+  char filename[1024];
+  int bits_per_block;
+  int blocks_per_file;
+  int64_t bounds[PIDX_MAX_DIMENSIONS];
+  double transform[16];
+  char bitSequence[512];
+  char bitPattern[512];
+  char filename_template[1024];                                         ///< Depends on the time step
+  
+  int64_t reg_patch_size[PIDX_MAX_DIMENSIONS];
+  
+  int compression_type;                                               ///< counter to enable/disable (1/0) compression
+  int enable_rst;                                               ///< counter to enable/disable (1/0) compression
+  /// 0 No aggregation
+  /// 1 Hybrid aggregation
+  /// 2 Only aggregation
+  int enable_agg;                                               ///< counter to enable/disable (1/0) compression
+
+  int compression_factor;
+  int compression_bit_rate;
+  int64_t chunk_size[PIDX_MAX_DIMENSIONS];                              ///< size of the block at which compression is applied eg. (4x4x4)
+                                                                        ///< the current compression schemes only work in three dimensions
+  int64_t chunked_bounds[PIDX_MAX_DIMENSIONS];                ///< Compressed global extents
+};
+typedef struct idx_file_struct* idx_dataset;
+
+
+/// idx_dataset_derived_metadata
+struct idx_dataset_derived_metadata_struct
+{
+  int dimension;
+  int samples_per_block;
+  int maxh;
+  int max_file_count;
+  
+  int fs_block_size;
+  off_t start_fs_block;
+
+  Agg_buffer **agg_buffer;
+
+  int dump_agg_info;
+  char agg_dump_dir_name[512];
+  int dump_io_info;
+  char io_dump_dir_name[512];
+
+  int color;
+  int idx_count[PIDX_MAX_DIMENSIONS];          ///< Number of idx files in each dimensions
+
+  int var_pipe_length;
+  int parallel_mode;
+
+  //extents of meta-data
+  int64_t *rank_r_offset;                                                   ///< Offset of variables in each dimension
+  int64_t *rank_r_count;                                                    ///< Count of variables in each dimension
+  
+  //int staged_aggregation;
+  int agg_type;
+  int layout_count;
+  int reduced_res_from;
+  int reduced_res_to;
+
+  PIDX_time time;
+};
+typedef struct idx_dataset_derived_metadata_struct* idx_dataset_derived_metadata;
+
+
+
+struct idx_debug_info_struct
+{
+  int debug_rst;                               ///< Debug restructuring phase, works only on the test application
+  int debug_hz;                                ///< Debug HZ encoding phase, works only on the test application
+
+  /// Flags set by user
+  int debug_do_rst;                            ///< User controlled flag to activate/deactivate restructuring phase
+  int debug_do_chunk;                          ///< User controlled flag to activate/deactivate chunking phase
+  int debug_do_compress;                       ///< User controlled flag to activate/deactivate compression
+  int debug_do_hz;                             ///< User controlled flag to activate/deactivate hz encoding phase
+  int debug_do_agg;                            ///< User controlled flag to activate/deactivate aggregation phase
+  int debug_do_io;                             ///< User controlled flag to activate/deactivate I/O phase
+};
+typedef struct idx_debug_info_struct* idx_debug;
+
+#endif
