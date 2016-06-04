9ab5c9629691d150a036b324f8cce92af1119286
sidharth87 <kumar.sidharth87@gmail.com>
2016-06-04 01:10:58 -0600

    benchmarking on mira with aggregating all fields

--- tutorial/1_PIDX_Basics_Dumps/checkpoint_simple.c
+++ tutorial/1_PIDX_Basics_Dumps/checkpoint_simple.c
@@ -457,8 +457,8 @@ int main(int argc, char **argv)
         break;
     }
 
-    ret = PIDX_debug_disable_agg(file);
-    if (ret != PIDX_success)  terminate_with_error_msg("PIDX_debug_output");
+    //ret = PIDX_debug_disable_agg(file);
+    //if (ret != PIDX_success)  terminate_with_error_msg("PIDX_debug_output");
 
     //ret = PIDX_debug_disable_io(file);
     //if (ret != PIDX_success)  terminate_with_error_msg("PIDX_debug_output");
