3f29bcb0913897ce77d4f0cd18a2c8ccd5a45141
sidharth kumar <kumar.sidharth87@gmail.com>
2017-03-18 22:25:52 -0600

    setup for block compression

--- pidx/core/PIDX_agg/PIDX_agg.c
+++ pidx/core/PIDX_agg/PIDX_agg.c
@@ -10,6 +10,7 @@ static PIDX_return_code one_sided_data_com(PIDX_agg_id id, Agg_buffer ab, int la
 static PIDX_return_code aggregate(PIDX_agg_id id, int variable_index, unsigned long long hz_start_index, unsigned long long hz_count, unsigned char* hz_buffer, int buffer_offset, Agg_buffer ab, PIDX_block_layout lbl, int MODE, int layout_id);
 static int intersectNDChunk(Ndim_patch A, Ndim_patch B);
 static PIDX_return_code decompress_aggregation_buffer(PIDX_agg_id id, Agg_buffer ab);
+static PIDX_return_code block_wise_compression(PIDX_agg_id id, Agg_buffer ab, PIDX_block_layout lbl);
 
 struct PIDX_agg_struct
 {
@@ -619,6 +620,9 @@ PIDX_return_code PIDX_agg_global_and_local(PIDX_agg_id id, Agg_buffer ab, int la
   ret = decompress_aggregation_buffer(id, ab);
   if (ret != MPI_SUCCESS) report_error(PIDX_err_agg, __FILE__, __LINE__);
 
+  ret = block_wise_compression(id, ab, lbl);
+  if (ret != MPI_SUCCESS) report_error(PIDX_err_agg, __FILE__, __LINE__);
+
   return PIDX_success;
 }
 
@@ -693,6 +697,21 @@ static PIDX_return_code decompress_aggregation_buffer(PIDX_agg_id id, Agg_buffer
   if (ab->buffer_size != 0)
   {
     //decompress buffer ab->buffer
+  }
+  return PIDX_success;
+}
+
+
+static PIDX_return_code block_wise_compression(PIDX_agg_id id, Agg_buffer ab, PIDX_block_layout lbl)
+{
+  PIDX_variable_group var_grp = id->idx->variable_grp[id->gi];
+  if (ab->buffer_size != 0)
+  {
+    int i;
+    for (i = 0; i < lbl->bcpf[ab->file_number]; i++)
+    {
+      //compress buffer ab->buffer + i * id->idx_d->samples_per_block * var_grp->variable[ab->var_number]->bpv/8
+    }
   }
   return PIDX_success;
 }
