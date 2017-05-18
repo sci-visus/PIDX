5e0dd5712f41bad1a0d02dba8306c74694c9cb8e
Steve Petruzza <spetruzza@sci.utah.edu>
2017-05-17 19:38:28 -0600

    Travis: new script (testing)

--- ci/travis/install_mpi.sh
+++ ci/travis/install_mpi.sh
@@ -12,26 +12,32 @@ if [ "$TRAVIS_OS_NAME" == "osx" ]; then
         brew install open-mpi
     fi
 else
-    # for Ubuntu builds install OpenMPI from source
-    # check to see if OpenMPI is cached from previous build
-    if [ -f "openmpi/bin/mpirun" ] && [ -f "openmpi-2.0.1/config.log" ]; then
-	echo "Using cached OpenMPI"
-	echo "Configuring OpenMPI"
-	cd openmpi-2.0.1
-	./configure --prefix=$TRAVIS_BUILD_DIR/openmpi CC=$C_COMPILER CXX=$CXX_COMPILER &> openmpi.configure
-    else
-        # install OpenMPI from source
-	echo "Downloading OpenMPI Source"
-	wget https://www.open-mpi.org/software/ompi/v2.0/downloads/openmpi-2.0.1.tar.gz
-	tar zxf openmpi-2.0.1.tar.gz
-	echo "Configuring and building OpenMPI"
-	cd openmpi-2.0.1
-	./configure --prefix=$TRAVIS_BUILD_DIR/openmpi CC=$C_COMPILER CXX=$CXX_COMPILER &> openmpi.configure
-	make -j4 &> openmpi.make
-	make install &> openmpi.install
-	cd ..
-    fi
-    # recommended by Travis CI documentation to unset these for MPI builds
+
+  if [ -f mpich/lib/libmpich.so ]; then
+    echo "libmpich.so found -- nothing to build."
+  else
+    echo "Downloading mpich source."
+    wget http://www.mpich.org/static/downloads/3.2/mpich-3.2.tar.gz
+    tar xfz mpich-3.2.tar.gz
+    rm mpich-3.2.tar.gz
+    echo "configuring and building mpich."
+    cd mpich-3.2
+    ./configure \
+        --prefix=`pwd`/../mpich \
+        --enable-static=false \
+        --enable-alloca=true \
+        --disable-long-double \
+        --enable-threads=single \
+        --enable-fortran=no \
+        --enable-fast=all \
+        --enable-g=none \
+        --enable-timing=none
+    make -j4
+    make install
+    cd -
+    rm -rf mpich-3.2
+fi
+
     test -n $CC && unset CC
     test -n $CXX && unset CXX
 fi
