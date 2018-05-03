4cd0f103cb484460aed0888407463f5075ce7ce2
spetruzza <steve.petruzza@gmail.com>
2018-05-03 17:29:10 -0600

    fix pathname for local partitioned non relative path

--- pidx/io/idx/local_partition/local_partition_generic_read.c
+++ pidx/io/idx/local_partition/local_partition_generic_read.c
@@ -329,15 +329,15 @@ static PIDX_return_code read_block(PIDX_io file, int vi, int p, int block_number
   directory_path = malloc(sizeof(*directory_path) * PATH_MAX);
   memset(directory_path, 0, sizeof(*directory_path) * PATH_MAX);
 
+  char full_path_file_name[PATH_MAX];
   char *lastdir = strrchr(file->idx->filename, '/');
   if (lastdir != NULL && file_name[0] == '.') { // if using relative paths use absolute path
     strncpy(directory_path, file->idx->filename, lastdir - file->idx->filename + 1);
+    sprintf(full_path_file_name, "%s/%s", directory_path,file_name);
   }
-  else
-    strncpy(directory_path, file->idx->filename, strlen(file->idx->filename) - 4); 
-
-  char full_path_file_name[PATH_MAX];
-  sprintf(full_path_file_name, "%s/%s", directory_path,file_name);
+  else{ 
+    sprintf(full_path_file_name, "%s", file_name);
+  }
 
   // open the binary file
   if (MPI_File_open(MPI_COMM_SELF, full_path_file_name, MPI_MODE_RDONLY, MPI_INFO_NULL, &fp) != MPI_SUCCESS)
